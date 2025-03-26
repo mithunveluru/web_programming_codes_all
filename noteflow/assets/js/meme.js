be22718418f6406f70666e2e6ea7b76878ffbef9
mithunveluru <you@example.com>
2025-03-26 19:24:22 +0530

    Update assets and settings

--- noteflow/assets/js/meme.js
+++ noteflow/assets/js/meme.js
@@ -3,7 +3,16 @@ document.addEventListener("DOMContentLoaded", function () {
         "images/1ms577a24vqe1.jpeg",
         "images/2c2ohahakwqe1.png",
         "images/4u4xqfxbpvqe1.png",
-        "images/b3pmjb0rfxqe1.png"
+        "images/4u4xqfxbpvqe1.png",
+        "images/mem1.png",
+        "images/mem2.jpg",
+        "images/mem3.jpeg",
+        "images/mem21.jpeg",
+        "images/mem456.jpeg",
+        "images/mem45.jpeg",
+        "images/meme222.jpeg",
+        "images/memem344.jpeg",
+        "images/yuki.jpeg"
     ];
 
     const memeImg = document.getElementById("meme-image");
